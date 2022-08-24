385740454f4c346232db83cdc3de08441b62d6bf
dherajsaah <dheerajsaah248@gmail.com>
2022-08-24 12:43:02 +0530

    [Dheeraj] Add: Diplaying TotalEmpWage

--- EmpWageComputation/EmployeeWage.cs
+++ EmpWageComputation/EmployeeWage.cs
@@ -8,8 +8,10 @@ namespace EmpWageComputation
 {
     internal class EmployeeWage
     {
+        const int IS_FULL_TIME = 0, WAGE_PER_HOUR = 20, FULL_TIME_HR = 8;
         public void Attendence()
         {
+
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == 0)
@@ -21,5 +23,18 @@ namespace EmpWageComputation
                 Console.WriteLine("Employee is Absent");
             }
         }
+        public void CalculateEmpWage()
+        {
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                int totalEmpWage = WAGE_PER_HOUR * FULL_TIME_HR;
+                Console.WriteLine(totalEmpWage);
+            }
+        }
+
+         
     }
 }
