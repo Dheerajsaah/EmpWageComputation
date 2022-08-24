53e32e33153a2ccddc6f46dd03a584243161f0e0
dherajsaah <dheerajsaah248@gmail.com>
2022-08-24 16:48:25 +0530

    [Dheeraj] Add: Displaying Wage For Month Using Case

--- EmpWageComputation/EmployeeWage.cs
+++ EmpWageComputation/EmployeeWage.cs
@@ -27,7 +27,7 @@ namespace EmpWageComputation
         public void CalculateEmpWage()
         {
             Random random = new Random();
-            for (int i = 0; i < WORKING_DAYS; i++)
+            for (int i = 0; i < WORKING_DAYS && empHrs<100; i++)
             {
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
